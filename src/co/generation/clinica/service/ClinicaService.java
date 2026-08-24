533b071c8014d80a3df2f2508d0bf34f26c1ee6a
ValeriaDiaz <valeriadiazbel@gmail.com>
2026-08-24 14:06:09 -0500

    turnos medico

--- src/co/generation/clinica/service/ClinicaService.java
+++ src/co/generation/clinica/service/ClinicaService.java
@@ -1,4 +1,29 @@
+
 package co.generation.clinica.service;
 
-public class ClinicaService {
+import co.generation.clinica.interfaces.Consultable;
+import co.generation.clinica.model.Medico;
+import co.generation.clinica.model.Turno;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ClinicaService implements Consultable {
+
+    public List<Turno>turnos= new ArrayList<>();
+
+    @Override
+    public List <Turno> buscarPorMedico(Medico medico); {
+        List<Turno> resultados = new ArrayList<>();
+
+        if (medico==null){
+            return resultados;
+        }
+    }
+    for (Turno m:this.turnos){
+        if (m.getMedico()!= null && m.getMedico().equals(medico)){
+            resultados.add(m);
+        }
+    }
+    return resultados;
 }
