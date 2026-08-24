bbf8d2a653a57196db0f206e697af5f0ffd61ac4
ThomasMontaño <viraclipscolo@gmail.com>
2026-08-24 15:10:46 -0500

    Metodos de Paciente

--- src/co/generation/clinica/service/ClinicaService.java
+++ src/co/generation/clinica/service/ClinicaService.java
@@ -13,6 +13,68 @@ import java.util.List;
 public class ClinicaService implements Consultable {
 
     public List<Turno> turnos = new ArrayList<>();
+    public List<Paciente> pacientes = new ArrayList<>();
+
+
+    public void registrarPaciente(Paciente a){
+        if (a == null || !a.esValido()) {
+            System.out.println("Error-- El paciente contiene datos nulos o no validos");
+            return;
+        }
+
+        if (pacientes.contains(a)) {
+            System.out.println("Error: Ya existe un paciente registrado con la cedula " + a.getCedula());
+            return;
+        }
+
+        //Asignamos el id
+
+        int nuevoId = pacientes.stream()
+                .mapToInt(Paciente::getId)
+                .max()
+                .orElse(0) + 1;
+        a.setId(nuevoId);
+
+        // agregamos a la lista
+        pacientes.add(a);
+
+        // mostramos en pantalla
+        System.out.println("Paciente registrado: " + a.getDatosRegistro());
+
+    }
+
+    public Paciente buscarPorCedula(String cedula) {
+        if (cedula == null) return null;
+
+        for (Paciente paciente : pacientes) {
+            if (cedula.equals(paciente.getCedula())) {
+                return paciente;
+            }
+        }
+        return null;
+    }
+
+    public void listarPacientes() {
+        if (pacientes.isEmpty()) {
+            System.out.println("No hay pacientes registrados en el sistema.");
+            return;
+        }
+
+        // Copia de la lista original
+        List<Paciente> copiaPacientes = new ArrayList<>(pacientes);
+
+        // Ordenar por apellido y luego por nombre
+        copiaPacientes.sort(Comparator.comparing(Paciente::getApellido)
+                .thenComparing(Paciente::getNombre));
+
+        for (Paciente p : copiaPacientes) {
+            System.out.println(p);
+        }
+    }
+
+
+
+
 
     @Override
     public List<Turno> buscarPorMedico(Medico medico) {
@@ -31,6 +93,7 @@ public class ClinicaService implements Consultable {
         return resultados;
     }
 
+
     @Override
     public List<Turno> listarTurnosDelDia(LocalDate fecha) {
         List<Turno> resultados = new ArrayList<>();
@@ -42,7 +105,6 @@ public class ClinicaService implements Consultable {
             }
         }
 
-        // Se ordena la lista una sola vez fuera del bucle
         resultados.sort(Comparator.comparing(Turno::getFechaHora));
         return resultados;
     }
